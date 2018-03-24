728999d884407cced6d955cf5d9923dec728ab1e
Dikla Aharoni Rikovitch <dikla.aharoni@gmail.com>
2018-03-24 12:50:37 -0700

    finished reverse words

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,4 +1,34 @@
+require 'pry'
 # A method to reverse each word in a sentence, in place.
 def reverse_words(my_words)
-  raise NotImplementedError
+  return nil if my_words == nil
+  return my_words if my_words.length == 1
+  index = 0
+
+  while index < my_words.length
+    while my_words[index] == " "
+      index += 1
+    end
+    j = index
+    while my_words[j] != " " && j < my_words.length - 1
+      j += 1
+    end
+    n = index
+    m = j
+    while n < m
+      while my_words[n] == " "
+        n += 1
+      end
+      while my_words[m] == " "
+        m -= 1
+      end
+      temp = my_words[n]
+      my_words[n] = my_words[m]
+      my_words[m] = temp
+      n += 1
+      m -= 1
+    end
+    index = j + 1
+  end
+  return my_words
 end
